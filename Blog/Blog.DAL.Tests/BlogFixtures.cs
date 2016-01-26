1297d69ee2362bfda1039015a302d9094cf09184
unknown <student@komp055.nmkzllab>
2016-01-26 16:47:14 +0100

    second commit

---
+++ Blog/Blog.DAL.Tests/BlogFixtures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TDD.DbTestHelpers.Yaml;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+
+namespace Blog.DAL.Tests
+{
+    public class BlogFixtures : YamlDbFixture<BlogContext, BlogFixturesModel>
+    {
+        public BlogFixtures()
+        {
+            SetYamlFiles("posts.yml");
+        }
+    }
+
+    public class BlogFixturesModel
+    {
+        public FixtureTable<Post> Posts { get; set; }
+    }
+
+
+}
---
+++ Blog/Blog.DAL.Tests/RepositoryTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Blog.DAL.Infrastructure;
+using Blog.DAL.Model;
+using Blog.DAL.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TDD.DbTestHelpers.Core;
+
+namespace Blog.DAL.Tests
+{
+    [TestClass]
+    public class RepositoryTests : DbBaseTest<BlogFixtures>
+    {
+        [TestMethod]
+        public void GetAllPost_TwoPostInDb_ReturnOnePost()
+        {
+            // arrange
+            var context = new BlogContext();
+            context.Database.CreateIfNotExists();
+            //context.Posts.ToList().ForEach(x => context.Posts.Remove(x));
+            //context.Posts.Add(new Post { 
+            //    Author = "test",
+            //    Content = "test test test.."
+            //});
+            context.SaveChanges();
+            var repository = new BlogRepository();
+            // act
+            var result = repository.GetAllPosts();
+            // assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        //[TestMethod]
+        //public void GetAllPost_TwoPostsInDb_ReturnTwoPost()
+        //{
+        //    // arrange
+        //    var context = new BlogContext();
+        //    context.Database.CreateIfNotExists();
+        //    var repository = new BlogRepository();
+        //    // act
+        //    var result = repository.GetAllPosts();
+        //    // assert
+        //    Assert.AreEqual(2, result.Count());
+        //}
+    }
+}
